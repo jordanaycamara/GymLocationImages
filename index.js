b8e072c2c79d1c230765b2c117e5a277bc0dbab9
Jordan Camara <jcamara@autosoftdms.com>
2018-04-08 23:45:43 -0400

    Googol Settings!

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const fs = require('fs'),
-	request = require('then-request'),
-	gyms = require('PgP-Data/data/gyms'),
-	googleSettings = require('./google-settings'),
-	settings = require('./settings');
+    request = require('then-request'),
+    gyms = require('PgP-Data/data/gyms'),
+    googleSettings = require('./google-settings'),
+    settings = require('./settings');
 
 if (!fs.existsSync('./images')) {
-	fs.mkdirSync('./images');
+    fs.mkdirSync('./images');
 }
 
 gyms
-	.filter(gym => !fs.existsSync(`./images/${gym.gymId}.png`))
-	.forEach(async gym => {
-		const id = gym.gymId,
-			latitude = gym.gymInfo.latitude,
-			longitude = gym.gymInfo.longitude,
-			width = settings.width,
-			height = settings.height,
-			scale = settings.scale,
-			zoom = settings.zoom,
-			googleKey = googleSettings.google_api_key,
-			path = '/maps/api/staticmap?' +
-				`size=${width}x${height}&` +
-				`scale=${scale}&` +
-				`zoom=${zoom}&` +
-				`markers=color:red|${latitude},${longitude}&` +
-				`key=${googleKey}`;
+    .filter(gym => !fs.existsSync('./images/${gym.gymId}.png'))
+    .forEach(async gym => {
+        const id = gym.gymId,
+            latitude = gym.gymInfo.latitude,
+            longitude = gym.gymInfo.longitude,
+            width = settings.width,
+            height = settings.height,
+            scale = settings.scale,
+            zoom = settings.zoom,
+            googleKey = googleSettings.google_api_key,
+            path = '/maps/api/staticmap?' +
+            'size=${width}x${height}&' +
+            'scale=${scale}&' +
+            'zoom=${zoom}&' +
+            'markers=color:red|${latitude},${longitude}&' +
+            'key=${googleKey}';
 
-		await request('GET', `https://maps.googleapis.com${path}`)
-			.done(result => fs.writeFileSync(`./images/${id}.png`, result.getBody()), 'binary');
-	});
+        await request('GET', 'https://maps.googleapis.com${path}')
+            .done(result => fs.writeFileSync('./images/${id}.png', result.getBody()), 'binary');
+    });
